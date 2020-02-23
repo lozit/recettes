833645dcf5742d7b1e585493308452e6b23fe5ba
Guillaume Ferrari <guillaume.ferrari@gmail.com>
2020-02-24 00:14:44 +0100

    Activation PWA / gatsby-plugin-offline

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
     `gatsby-plugin-remove-fingerprints`,
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-netlify`,
