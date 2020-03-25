26c02857d6541f0237343e7352d451748c35ac34
Guillaume Ferrari <guillaume.login@protonmail.com>
2020-03-25 23:40:45 +0100

    Access-Control-Allow-Origin

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,7 @@ module.exports = {
       resolve: `gatsby-plugin-netlify`,
       options: {
         headers: {
-          "/*": ["Access-Control-Allow-Origin: https://recettes.heyjoe.fr/"],
+          "/*": ["Access-Control-Allow-Origin: *"],
         },
       },
     },
